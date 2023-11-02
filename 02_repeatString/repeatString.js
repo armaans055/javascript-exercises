ab35a90213ffb00acba32f8d5220aa9a070f7862
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-01 23:30:33 -0700

    Finished repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let newString = ''
+    for(i=0;i<number;i++){
+        newString += string
+    }
+    return newString
 };
 
 // Do not edit below this line
