b7d45df298edbad0104554361b3fe3f149455a32
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-02 00:12:39 -0700

    Finished first test of removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, replace) {
+    let replacementLocation = array.indexOf(replace)
+    array.splice(replacementLocation,1)
+    return array
 };
 
 // Do not edit below this line
