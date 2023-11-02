68b77b62ab6c118fe97a208bede7214f40c05c5e
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-01 23:50:56 -0700

    repeatString skip deleted

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    newString = string.split('')
+    reversed = newString.reverse()
+    finalString = newString.join('')
+    return finalString
 };
 
 // Do not edit below this line
