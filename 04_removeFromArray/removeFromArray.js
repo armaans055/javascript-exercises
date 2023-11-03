0e6b6fed5d52d6d58625331b4440e487d54b297e
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-03 16:25:54 -0700

    Made it remove one element if it is not an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,15 @@
-const removeFromArray = function(array, replace) {
-    let replacementLocation = array.indexOf(replace)
-    array.splice(replacementLocation,1)
-    return array
+const removeFromArray = function(array, remove) {
+    if(Array.isArray(remove)){
+
+    }else{
+        let removalLocation = array.indexOf(remove)
+        array.splice(removalLocation,1)
+        return array
+    }
+   //Write a blanket if statement to determine if it is an array, if not continue and remove only one
+   //If it is an array, assign a new value to the removing components
+   //Iterate through and splice the array to remove those values
+   //array.splice(remove , 1)
 };
 
 // Do not edit below this line
