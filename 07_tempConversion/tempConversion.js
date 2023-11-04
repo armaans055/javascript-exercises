80d203998c478658650a9b78582585d5ff3bd1d7
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-04 14:25:41 -0700

    Finished tempConversion.spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) * 5/9
+  let celsiusRounded = Math.round(celsius *10)/10
+  return celsiusRounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let farenheit = (temp * (9/5)) + 32
+  let farenheitRounded = Math.round(farenheit *10)/10
+  return farenheitRounded
 };
 
 // Do not edit below this line
