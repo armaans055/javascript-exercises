75d6cec3b9e4ccff6900c2107d70fd1317cf0e1f
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-04 13:24:07 -0700

    Finished removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,8 @@
-const removeFromArray = function(array, remove) {
-    if(Array.isArray(remove)){
-
-    }else{
-        let removalLocation = array.indexOf(remove)
-        array.splice(removalLocation,1)
-        return array
-    }
-   //Write a blanket if statement to determine if it is an array, if not continue and remove only one
-   //If it is an array, assign a new value to the removing components
-   //Iterate through and splice the array to remove those values
-   //array.splice(remove , 1)
+const removeFromArray = function(arr, ...args) {
+    let a = arr
+    let b = args
+    let c = a.filter((d) => !b.includes(d));
+    return c;
 };
 
 // Do not edit below this line
