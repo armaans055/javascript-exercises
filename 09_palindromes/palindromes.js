209d5cb4b46cb4e996d554a038a6e1c28df0aa8e
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-13 22:28:15 -0800

    Finished palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const string = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()' ']/g,"")
+    let splitString = string.split("")
+    let reversedString = splitString.reverse();
+    let finalString = reversedString.join("");
+    if(string == finalString){
+        return true
+    }else{
+        return false
+    }
 };
 
 // Do not edit below this line
