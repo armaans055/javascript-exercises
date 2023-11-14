a0c741232f070bdd597f8770353c0103637053f4
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-13 22:09:31 -0800

    Finished calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
-};
+const add = (a,b) => {return a + b}
 
-const subtract = function() {
-	
-};
+const subtract = (a,b) => {return a - b}
 
-const sum = function() {
-	
-};
+const sum = function(array){
+  return array.reduce((a,b) =>  a + b, 0)
+}
 
-const multiply = function() {
+const multiply = function(array){
+  return array.reduce((a,b) => a * b)
+}
 
-};
+const power = (a,b) => {return a ** b}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0){  
+  return 1
+  }else{
+    product = 1;
+    for(let i=num; i > 0; i--){
+    product *= i
+    }
+  return product
+  }
 };
 
 // Do not edit below this line
