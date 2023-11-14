bfca4fcd7d6a52d6a11437d247b516d921370a1e
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-14 10:58:11 -0800

    Finished all

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let a = 0;
+    let b = 1;
+    if(num < 0){
+        return "OOPS"
+    }else{
+        for(i=2; i<=num; i++){
+            let c = a+b;
+            a = b;
+            b = c;
+        }
+    return b
+    }
 };
 
+
+//Derive the preceding fibonacci series, from a given number
+//
 // Do not edit below this line
 module.exports = fibonacci;
